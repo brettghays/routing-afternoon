6d47081f45f8591fe2c65df3ef9d614dc7b0c08b
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-01-09 22:08:36 -0700

    finished afternoon...almost.

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -13,16 +13,16 @@ export default class Student extends Component {
     componentDidMount(){
       return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
         .then(res => {
-          console.log(res.data)
           this.setState({studentInfo: res.data})
+          //console.log(this.state.studentInfo)
         })
-        .catch(err => console.log(err))
+        //.catch(err => console.log(err))
     }
 
     render() {
       return (
         <div className="box">
-          <h1>Student</h1>
+          <h1>Student:</h1>
           <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
           <h3>Grade: {this.state.studentInfo.grade}</h3>
           <h3>Email: {this.state.studentInfo.email}</h3>
